b6c7b801651f8a5becff60df4b0cbe076495e686
chm031313 <75466625+koreankimchi123@users.noreply.github.com>
2025-11-16 22:21:35 +0900

    run +grid only

--- build_traffic.sh
+++ build_traffic.sh
@@ -1,13 +1,24 @@
 #! /bin/bash  
+set -euo pipefail
 # Main information
 echo "Legal traffic: build"
 echo ""
 echo "Generating traffic..."
 
 cd skyfall || exit 1
-total_timeslots=$1
-cpu_cores=$2
-seq 0 $total_timeslots | xargs -n 1 -P $cpu_cores -I{} bash -c "sudo python3 generate_flow_grid.py {} & sudo python3 generate_flow_circle.py {}"
+# total_timeslots=$1
+# cpu_cores=$2
+# seq 0 $total_timeslots | xargs -n 1 -P $cpu_cores -I{} bash -c "sudo python3 generate_flow_grid.py {} & sudo python3 generate_flow_circle.py {}"
+
+total_timeslots="${1:?missing total_timeslots}"
+cpu_cores="${2:?missing cpu_cores}"
+
+
+# 각 타임슬롯마다 +grid만 실행
+seq 0 "$total_timeslots" | xargs -n 1 -P "$cpu_cores" -I{} \
+  bash -c '
+    sudo python3 generate_flow_grid.py "$1"
+  ' _ {}
 cd .. || exit 1
 
 # Calculation finished
